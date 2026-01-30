9766c3a9cb7f63566f79826e3bab657949860135
MijailSM <mijail.sazhin@estudiante.uam.es>
2026-01-30 13:12:57 +0100

    protocol extractor first try

--- csv_processer.py
+++ csv_processer.py
@@ -1,8 +1,9 @@
-import sys
 import tarfile
 import pandas as pd
 import io
 import os
+import argparse
+import ast
 
 def mergecsvs(csvs: list) -> pd.DataFrame:
     
@@ -15,7 +16,20 @@ def mergecsvs(csvs: list) -> pd.DataFrame:
     
     return merged
 
-def attack_bening_cleanup(df: pd.DataFrame):
+def protocol_extractor(df: pd.DataFrame) -> pd.DataFrame:
+    df["protocols_list"] = df["network_protocols_all"].apply(
+        lambda x: ast.literal_eval(x) if isinstance(x, str) and x.startswith('[') else []
+    )
+    target_protocols = ['tcp', 'udp', 'icmp', 'arp', 'tls', 'http', 'dns']
+    for protocol in target_protocols:
+        df[f"proto_{protocol}"] = df["protocols_list"].apply(
+            lambda x: 1 if protocol in x else 0
+        )
+    df = df.drop(["protocols_list", "network_protocols_all"], axis=1)
+    print(f"Se han procesado los siguientes protocolos: {target_protocols}")
+    return df
+
+def attack_bening_basic_cleanup(df: pd.DataFrame) -> pd.DataFrame:
     df['target'] = df['label1'].map({'benign': 0, 'attack': 1})
     df = df.drop(['device_name', 'device_mac', 'label_full','label2', 'label3', 'label4', 'timestamp'], axis=1)
     
@@ -27,34 +41,46 @@ def attack_bening_cleanup(df: pd.DataFrame):
     return df
 
 def main():
-    if len(sys.argv) < 2:
-        print("Usage: mergecsv [name1] [name2] ... [namen]")
-        return
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input", nargs="+", help="Archivos tar o csv de entrada")
+    parser.add_argument("-c", "--cleanup", action="store_true", help="Realizar limpieza de la unión")
+    parser.add_argument("-n", "--name", type=str, default="csv/Merged_DF.csv", help="Nombre del csv final")
+    args = parser.parse_args()
     
     csvs = []
-    for name in sys.argv[1:]:
-        if name.endswith('.tar.xz'):
-            print(f'Descomprimiendo {name}...')
-            with tarfile.open(name, "r:xz") as tar:
-                member = next(m for m in tar.getmembers() if m.name.endswith('.csv'))
-                f = tar.extractfile(member)
-                df = pd.read_csv(io.BytesIO(f.read()))
-        else:
-            df = pd.read_csv(name)
+    for name in args.input:
+        try:
+            if name.endswith('.tar.xz'):
+                print(f'Descomprimiendo {name}...')
+                
+                with tarfile.open(name, "r:xz") as tar:
+                    member = next(m for m in tar.getmembers() if m.name.endswith('.csv'))
+                    f = tar.extractfile(member)
+                    df = pd.read_csv(io.BytesIO(f.read()))
+            else:
+                df = pd.read_csv(name)
+        except Exception as e:
+            print(f"Error al procesar {name}: {e}")
+            return
+
         
         print(f"Cargando archivo: {name} || Columnas: {len(df.columns)}")
             
         csvs.append(df)
     
     merged = mergecsvs(csvs)
-    print("DataFrame unido, limpiando...")
-    merged = attack_bening_cleanup(merged)
+    if args.cleanup == True:
+        print("DataFrame unido, limpiando...")
+        merged = protocol_extractor(merged)
+        merged = attack_bening_basic_cleanup(merged)
+    os.makedirs("csv", exist_ok=True)
     
-    name = "csv/Cleaned_DF.csv"
-    if os.path.exists("csv/") == False:
-        os.mkdir("csv")
-    print(f"Dataframe generado y limpio, guardando como {name}...")
-    merged.to_csv(name, index=False)
+    if args.name.endswith(".csv") == False:
+        args.name = f"{args.name}.csv"
+    args.name = f"csv/{args.name}"
+    print(f"Dataframe nuevo, guardando como {args.name}...")
+    merged.to_csv(args.name, index=False)
     
 if __name__ == "__main__":
     main()
