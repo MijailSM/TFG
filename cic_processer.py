ad57abb27c8f03628b3529b93eafed44a29cae04
MijailSM <mijail.sazhin@estudiante.uam.es>
2026-04-03 19:49:38 +0200

    fix

--- cic_processer.py
+++ cic_processer.py
@@ -1,13 +1,17 @@
 import dask.dataframe as dd
 from dask.distributed import Client
 
-client = Client()
-print(f"Dashboard en {client.dashboard_link}")
+def process():
+    client = Client()
+    print(f"Dashboard en {client.dashboard_link}")
 
-# 1. Carga virtual del archivo (no ocupa RAM al inicio)
-df = dd.read_csv('/mnt/datos/AllCicMerged.csv')
+    # 1. Carga virtual del archivo (no ocupa RAM al inicio)
+    df = dd.read_csv('/mnt/datos/AllCicMerged.csv')
 
-# 2. Contar etiquetas de forma masiva usando todos los núcleos
-counts = df['label1'].value_counts().compute() 
+    # 2. Contar etiquetas de forma masiva usando todos los núcleos
+    counts = df['label1'].value_counts().compute() 
 
-print(counts)
+    print(counts)
+    
+if __name__ == "__main__":
+    process()
