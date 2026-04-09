597453222ac11f127e702355648c3ff59113f260
MijailSM <mijail.sazhin@estudiante.uam.es>
2026-04-09 20:09:33 +0200

    gr

--- cic_processer.py
+++ cic_processer.py
@@ -48,5 +48,6 @@ def balance_data():
         print("Muestreo completado y guardado.")
 
 if __name__ == "__main__":
-    with dask.config.set({'temporary_directory': '/mnt/datos/temp'}):
-        balance_data()
+    #with dask.config.set({'temporary_directory': '/mnt/datos/temp'}):
+    #    balance_data()
+    balance_data()
