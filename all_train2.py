948dd787f9555321c165a5baa44eb13e2bdb8a90
MijailSM <mijail.sazhin@estudiante.uam.es>
2026-05-06 10:30:33 +0200

    fix alltrain2

--- all_train2.py
+++ all_train2.py
@@ -30,14 +30,14 @@ def process(input, folder, tarea):
     X = df.drop(columns=['target'])
     y = df['target']
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
-    
+    """
     print("Realizando undersampling...")
     if tarea != '60clases':
         enn_1s = EditedNearestNeighbours(sampling_strategy=[0], n_neighbors=3)
     else:
         enn_1s = EditedNearestNeighbours(sampling_strategy=[4], n_neighbors=3)
     X_train, y_train = enn_1s.fit_resample(X_train, y_train)
-    
+    """
     scaler = StandardScaler()
     X_train_scaled = scaler.fit_transform(X_train)
     X_test_scaled = scaler.fit_transform(X_test)
