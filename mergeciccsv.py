b2f17d41c4b714337ae5fd60601dfd7d99f569cf
MijailSM <mijail.sazhin@estudiante.uam.es>
2026-05-06 09:22:11 +0200

    gei3

--- mergeciccsv.py
+++ mergeciccsv.py
@@ -3,7 +3,7 @@ import pandas as pd
 import glob
 
 files = glob.glob('/mnt/datos/output/balanced_csv_parts/*.csv')
-archivo_salida = 'mnt/datos/output/MergedBalanced.csv'
+archivo_salida = '/mnt/datos/output/MergedBalanced.csv'
 first = True
 for file in tqdm.tqdm(files, desc="Procesando csvs...", unit='file'):
     try:
